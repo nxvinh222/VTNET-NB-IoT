35a40bb3550b8b17ca868d76947f17c4ded9e553
unknown <nxvinh222@gmail.com>
2020-08-18 11:08:41 +0700

    Add: IntroVideo design

--- src/components/IntroVideo.js
+++ src/components/IntroVideo.js
@@ -1,26 +1,33 @@
 import React from "react"; 
-import { Container, ResponsiveEmbed, Card, Button } from "react-bootstrap"
+import { Container, ResponsiveEmbed, Card, Button, Row } from "react-bootstrap"
 import '../App.css';
 
 function IntroVideo() {
 
     return (
-        <Container> 
+        <Container > 
             <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
             <h1 class="display-4">Pricing</h1>
             <p class="lead">Watch the video to see how Internet of Things (IoT) could transform and revolutionise every business</p>
             </div>
-            <Card>
+
+
+            <Card className="p-3 py-3 shadow bg-white rounded">
                 <Card.Body>
-                    <ResponsiveEmbed aspectRatio="16by9">
-                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/tgbNymZ7vqY" allowfullscreen></iframe>
-                    </ResponsiveEmbed>
-                    {/* <Card.Title>Card Title</Card.Title> */}
-                    <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                    <Row>
+                        <ResponsiveEmbed className='col-md-8' aspectRatio="16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/tgbNymZ7vqY" allowfullscreen></iframe>
+                        </ResponsiveEmbed>
+                        {/* <Card.Title>Card Title</Card.Title> */}
+                        <Card.Text  className='col-md-3 m-3'>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            <br></br>
+                            <br></br>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Text>
+                        
+                    </Row>
                 </Card.Body>
             </Card>
             
