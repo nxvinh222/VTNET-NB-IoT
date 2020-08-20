c03682cc35e28891dc9c05a0db3029982cc0ad58
unknown <nxvinh222@gmail.com>
2020-08-20 15:04:14 +0700

    Fix: Carousel responsive

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Carousel, Container, Button } from "react-bootstrap"
 // import CarouselItem from "./CarouselItem"
 import '../App.css';
+import '../scss/main.scss'
 
 const carouselPicture =
     "https://upload.wikimedia.org/wikipedia/commons/5/52/Golden_sky_%284080648531%29.jpg"
@@ -17,13 +18,13 @@ function ControlledCarousel() {
         setData([
             {
                 title: "Arjuna",
-                text: "Expanding sacred domain. Domain secured. Divine punishment enforcement limits... All approved. By the wrath of Shiva, here ends thy life. Pashupata!",
+                text: "Expanding sacred domain. Domain secured.",
                 picture: "https://www.cdacollaborative.org/wp-content/uploads/2017/07/Biz-and-Peace-Carousel-Background-01.png",
                 href: "http://google.com"
             },
             {
-                title: "Altria Pendragon",
-                text: "Light, may you be released from the ends of the world. Split the heavens and tether the earth, anchor of the storm! Rhongomyniad!",
+                title: "Altria",
+                text: "Light, may you be released from the ends of the world. ",
                 picture: "https://www.cdacollaborative.org/wp-content/uploads/2017/07/Biz-and-Peace-Carousel-Background-01.png",
                 href: "http://google.com"
             }
@@ -33,16 +34,17 @@ function ControlledCarousel() {
 
     // Map Carousel's data
     const allItem = data.map((itemInfo) => (
-            <Carousel.Item className="carousel-top ">
+            <Carousel.Item className="">
                     <img
                     className="d-block w-100"
                     src={itemInfo.picture}
                     alt="Slide"
+                    style={{width: "871px", height: "226px"}}
                     />
                     <Carousel.Caption className="">
-                        <h3 className="display-4" style={{fontSize : "8vw"}}>{itemInfo.title}</h3>
-                        <p class="lead" style={{fontSize : "2vw"}}>{itemInfo.text}</p>
-                        <Button variant="primary" size="md" href={itemInfo.href} style={{fontSize : "2vw"}} active>
+                        <h3 className="" style={{fontFamily: "arial"}}>{itemInfo.title}</h3>
+                        <p className="lead" >{itemInfo.text}</p>
+                        <Button size="lg" href={itemInfo.href} id="carousel-top-button" active>
                             Primary button
                         </Button>{' '}
                     </Carousel.Caption>
@@ -57,9 +59,10 @@ function ControlledCarousel() {
 
     // Return all Carousel's items
     return (
-        <Container className="hero-text-carousel">
+        <Container className="hero-text-carousel" style={{width: "85vw"}}>
             <Carousel activeIndex={index} onSelect={handleSelect}>
                 {allItem}
+                
             </Carousel>
         </Container>
     );
