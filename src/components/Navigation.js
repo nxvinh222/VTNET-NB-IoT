ee5112a44d4366e303951207a6380a93abece60e
unknown <nxvinh222@gmail.com>
2020-08-21 08:35:11 +0700

    Fix: Navigator's responsive design

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -35,7 +35,7 @@ function Navigation() {
             <Row>
                 <h2 className="col-sm-10 py-3 pt-md-5 pb-md-4 mx-auto text-center display-4">See how we’re transforming lives and businesses</h2>
 
-                <Card className="col-lg-6 mx-auto border-0">
+                <Card className="col-lg-8 mx-auto border-0">
                     <Card.Header >
                         <Nav
                             variant="tabs"
