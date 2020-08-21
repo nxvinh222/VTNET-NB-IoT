d450f5941e6dadf061c00c21714f11db155c54ab
unknown <nxvinh222@gmail.com>
2020-08-21 14:17:29 +0700

    Add: TitleBanner and design

--- src/components/TitleBanner.js
+++ src/components/TitleBanner.js
@@ -9,10 +9,23 @@ class TitleBanner extends Component {
     return (
         <div className="" style={{ color: 'black', position: "relative"}}>     
             <div className="p-5" style={{backgroundColor: "#930800", color: "white"}}>
-                  <p className="display-4" >Did you know?</p>
-                  <p className="lead">Contact us and one of our IoT specialists will call you back</p>
+              <p className="display-4 text-center" >Did you know?</p>
+              <Row>
+                <div className="col-4 text-center">
+                  <i className="far fa-star fa-3x"></i>
+                  <p className="lead mt-3">Three Wise Men of the East. The Great God of the Norse. The Fruit of Wisdom. My intellect, my omnipotence, surpasses all forms of wisdom! Uomo Universale!</p>
+                </div>
+                <div className="col-4 text-center">
+                  <i class="fas fa-globe fa-3x"></i>
+                  <p className="lead mt-3">Noble Phantasm, Fragment Deployment. Grant me flames. The radiance of the morning sun! Burn! Devour! Anything and everything!</p>
+                </div> 
+                <div className="col-4 text-center">
+                  <i class="far fa-heart fa-3x"></i>
+                  <p className="lead mt-3">Those who cherish their lives, vanish with haste. From here on, the scorching heat will take you to the eighth circle of hell! Let the gods and Buddhas of the three realms return to ash!</p>
+                </div>
+              </Row>
+              
             </div>
-
         </div>
     );
   }
