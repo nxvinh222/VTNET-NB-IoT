5847f1ab5ea0acd1b979a6b5afb7ceb07090501c
unknown <nxvinh222@gmail.com>
2020-08-25 13:44:44 +0700

    Fix: SecondBanner design

--- src/components/SecondBanner.js
+++ src/components/SecondBanner.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Breadcrumb } from "react-bootstrap"
+import { Breadcrumb, Container } from "react-bootstrap"
 
 
 // const logoUri =
@@ -11,16 +11,22 @@ class SecondBanner extends Component {
   render() {
     return (
       <div>
-        <Breadcrumb className="">
-          <Breadcrumb.Item href="#">Viettel Business Home</Breadcrumb.Item>
-          <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
-            Solution
-                </Breadcrumb.Item>
-          <Breadcrumb.Item active>IoT</Breadcrumb.Item>
-        </Breadcrumb>
+
+          <Breadcrumb className="">
+            <Container>
+              <Breadcrumb.Item href="#" style={{display:"inline-block"}}>Viettel Business Home</Breadcrumb.Item>
+              <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/" style={{display:"inline-block"}}>
+                Solution
+                    </Breadcrumb.Item>
+              <Breadcrumb.Item style={{display:"inline-block"}} active>IoT</Breadcrumb.Item>
+            </Container>
+          </Breadcrumb>
+
         <div className="p-5" style={{ backgroundColor: "#00697d", color: "white" }}>
-          <h3 className="heading-3 text-left">Internet of Things is transforming lives and businesses</h3>
-          <p className="text-left lead">The Internet of Things (IoT) transforms industries around the world, and it’s not hard to see why. IoT solutions can help you unlock new revenue streams, improve efficiency and increase customer engagement and loyalty. Why risk being left behind? We can help you make the most of this powerful technology, delivering immediate benefits and setting you up for future success.</p>
+          <Container>
+            <h3 className="heading-3 text-left">Internet of Things is transforming lives and businesses</h3>
+            <p className="text-left lead">The Internet of Things (IoT) transforms industries around the world, and it’s not hard to see why. IoT solutions can help you unlock new revenue streams, improve efficiency and increase customer engagement and loyalty. Why risk being left behind? We can help you make the most of this powerful technology, delivering immediate benefits and setting you up for future success.</p>
+          </Container>
         </div>
       </div>
 
