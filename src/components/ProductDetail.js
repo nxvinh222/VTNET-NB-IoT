34d6aac7e057278c95e9f6b2386b4dcb195a9d85
unknown <nxvinh222@gmail.com>
2020-08-27 15:26:54 +0700

    Add: ProductDetail quantity input field

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,7 +1,16 @@
-import React from "react"
-import { Container, Row } from "react-bootstrap"
+import React, { useState } from "react"
+import { Container, Row, Button } from "react-bootstrap"
 
 function InsideProduct (props) {
+    // State
+    const [quantity, setQuantity] = useState(1)
+
+    const handleTextChange = (event) => {
+        setQuantity(event.target.value)
+        console.log(quantity);
+    }
+
+
     return (
         <div className="product-detail">
             <Container>
@@ -12,7 +21,8 @@ function InsideProduct (props) {
                         </a>
                     </div>
                     
-                    <div className="info col-6">
+                    <div className="info col-7">
+
                         <div className="top-info">
                             <div className="info-brand">
                                 <img
@@ -30,14 +40,43 @@ function InsideProduct (props) {
                                 <span className="font-weight-bold">777777777</span>
                             </div> */}
                         </div>
+
                         <div className="price price-font">
                                 <h3 className="" style={{color: "rgb(0, 105, 125)"}}>{(1000000).toLocaleString()}₫</h3>
-
                         </div>
+
                         <p className="description"
                             style={{fontSize: "0.9em"}}>
                             D'Éon was born at the Hôtel d'Uzès in Tonnerre, Burgundy, into a poor noble family. D'Éon's father, Louis d'Éon de Beaumont, was an attorney and director of the king's dominions, later mayor of Tonnerre and sub-delegate of the intendant of the généralité of Paris.
                         </p>
+
+                        <div className="add-to-card">
+                            
+                                <div className="quantity-input col-12">
+                                    <i 
+                                        className="fas fa-minus text-danger"
+                                        onClick={() => setQuantity(prevQuantity => prevQuantity>1 ? prevQuantity-1 : 1)}
+                                    ></i>
+                                    <input 
+                                        className="col-2 mx-3 text-center"
+                                        type="text"
+                                        name="quantity"
+                                        value={quantity}
+                                        onChange={handleTextChange}
+                                        style={{
+                                            outline: "none",
+                                            border: "none"
+                                        }}
+                                    />
+                                    <i 
+                                        className="fas fa-plus text-danger"
+                                        onClick={() => setQuantity(prevQuantity => prevQuantity+1)}
+                                    ></i>
+                                    
+                                </div>
+                                <Button className="col-3 m-4" href="#" variant="danger">Add to Card</Button>{' '}
+                          
+                        </div>
                     </div>
                 </Row>
             </Container>
