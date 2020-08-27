34d78a319406ec9eb60490b5d7d5a090fe95bb79
unknown <nxvinh222@gmail.com>
2020-08-27 10:26:41 +0700

    Fix: add PricingTable div classname

--- src/components/PricingTableFeature.js
+++ src/components/PricingTableFeature.js
@@ -8,68 +8,73 @@ function PricingTableFeature(props) {
             <Col>
                 <Card className="border-0 text-center">
                     {/* Title */}
-                    <Card.Body className="text-light bg-dark" style={{minHeight: "110px"}}>
-                        <div className="ml-5 mr-5">
-                            <Card.Title>{props.data.title}</Card.Title>
-                            <Card.Text style={{ fontSize: "0.7em" }}>{props.data.titleIntro}</Card.Text>
-                        </div>
+                    <div className="price-table-title">
+                        <Card.Body className="text-light bg-dark" style={{minHeight: "110px"}}>
+                            <div className="ml-5 mr-5">
+                                <Card.Title>{props.data.title}</Card.Title>
+                                <Card.Text style={{ fontSize: "0.7em" }}>{props.data.titleIntro}</Card.Text>
+                            </div>
+                        </Card.Body>
+                    </div>
 
-                    </Card.Body>
                     {/* Body */}
-                    <Card.Body>
-                        {/* Price */}
-                        <Card.Text className="mb-0" style={{ color: "#b7b4b4", fontSize: "19px" }}>
-                            Starting at
-                        </Card.Text>
-                        <Card.Title className="" style={{ fontSize: "35px" }}>{props.data.price.toLocaleString()}<sup>Đ</sup></Card.Title>
-                        {/* Detail */}
-                        
-                        <ul className="price-table-features pl-0 px-2" style={{
-                            listStyleType: "none",
-                            color: "#777777"
-                        }}>
-                            <li className="my-3">
-                                <i className="far fa-check-circle mr-1"></i>
-                                <span>
-                                    {props.data.detailsList[0]}
+                    <div className="price-table-body">
+                        <Card.Body>
+                            {/* Price */}
+                            <Card.Text className="mb-0" style={{ color: "#b7b4b4", fontSize: "19px" }}>
+                                Starting at
+                            </Card.Text>
+                            <Card.Title className="" style={{ fontSize: "35px" }}>{props.data.price.toLocaleString()}<sup>Đ</sup></Card.Title>
+                            {/* Detail */}
+                            
+                            <ul className="price-table-features pl-0 px-2" style={{
+                                listStyleType: "none",
+                                color: "#777777"
+                            }}>
+                                <li className="my-3">
+                                    <i className="far fa-check-circle mr-1"></i>
+                                    <span>
+                                        {props.data.detailsList[0]}
+                                    </span>
+                                </li>
+                                <div className="price-detail-top-border px-2"></div>
+                                <li className="my-3">
+                                    <i className="far fa-check-circle mr-1"></i>
+                                    <span>
+                                        {props.data.detailsList[1]}
+                                    </span>
+                                </li>
+                                <div className="price-detail-top-border"></div>
+                                <li className="my-3">
+                                    <i className="far fa-check-circle mr-1"></i>
+                                    <span>
+                                        {props.data.detailsList[2]}
                                 </span>
-                            </li>
-                            <div className="price-detail-top-border px-2"></div>
-                            <li className="my-3">
-                                <i className="far fa-check-circle mr-1"></i>
-                                <span>
-                                    {props.data.detailsList[1]}
+                                </li>
+                                <div className="price-detail-top-border"></div>
+                                <li className="my-3">
+                                    <i className="far fa-check-circle mr-1"></i>
+                                    <span>
+                                        {props.data.detailsList[3]}
                                 </span>
-                            </li>
-                            <div className="price-detail-top-border"></div>
-                            <li className="my-3">
-                                <i className="far fa-check-circle mr-1"></i>
-                                <span>
-                                    {props.data.detailsList[2]}
-                            </span>
-                            </li>
-                            <div className="price-detail-top-border"></div>
-                            <li className="my-3">
-                                <i className="far fa-check-circle mr-1"></i>
-                                <span>
-                                    {props.data.detailsList[3]}
-                            </span>
-                            </li>
-                            <div className="price-detail-top-border"></div>
-                            <li className="my-3">
-                                <i className="far fa-check-circle mr-1"></i>
-                                <span>
-                                {props.data.detailsList[4]}
-                            </span>
-                            </li>
-                        </ul>
+                                </li>
+                                <div className="price-detail-top-border"></div>
+                                <li className="my-3">
+                                    <i className="far fa-check-circle mr-1"></i>
+                                    <span>
+                                    {props.data.detailsList[4]}
+                                </span>
+                                </li>
+                            </ul>
 
-                        <Button className="my-3" variant="danger">Get Started</Button>{' '}
+                            <Button className="my-3" href={window.location.href + '/1'} variant="danger">Get Started</Button>{' '}
 
-                        <Card.Text className="mx-2 mt-3" style={{ color: "#707070", fontSize: "13px" }}>
-                            {props.data.detailsText}
-                        </Card.Text>
-                    </Card.Body>
+                            <Card.Text className="mx-2 mt-3" style={{ color: "#707070", fontSize: "13px" }}>
+                                {props.data.detailsText}
+                            </Card.Text>
+                        </Card.Body>
+                    </div>
+                    
                     <Card.Img variant="top" src="https://sodaq.com/wp-content/uploads/2019/11/SODAQ_WEBSHOP-min.jpg" />
                 </Card>
             </Col>
--- src/containers/InsideProductScreen.js
+++ src/containers/InsideProductScreen.js
@@ -1,16 +1,11 @@
 import React from "react"
 import TopNav from "../components/TopNav"
 import NavBar from "../components/NavBar"
-import Banner from "../components/Banner"
-import Carousel  from "../components/Carousel"
-import SecondBanner from "../components/SecondBanner"
-import IntroVideo from "../components/IntroVideo"
-import Navigation from "../components/Navigation"
-import IntroCards from "../components/IntroCards"
+import InsideProduct from "../components/InsideProduct"
 import ContactBanner from "../components/ContactBanner"
 import TitleBanner from "../components/TitleBanner"
 import Footer  from "../components/Footer"
-import PricingTable from "../components/PricingTable"
+
 
 function HomeScreen() {
 
@@ -18,9 +13,7 @@ function HomeScreen() {
         <div>
             <TopNav />
             <NavBar />
-            <Banner bannerText="Take the development in-house"/>
-            <PricingTable />
-            <IntroCards />
+            <InsideProduct />
             <ContactBanner />
             <TitleBanner />
             <Footer />
