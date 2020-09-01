ca213fcd17d9dd2f97d049ee8d0179d3c92dceb6
unknown <nxvinh222@gmail.com>
2020-09-01 10:49:29 +0700

    Fix: navbar-top design bug

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,8 @@ import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-boots
 
 function NavBar() {
     return(
-        <div className="navbar">
-            <Navbar bg="light" expand="lg">
+        <div className="navbar-top">
+            <Navbar bg="light" expand="sm">
                 <Navbar.Brand href="/">
                     <img
                         src={window.location.origin + '/viettel_logo.png'}
@@ -13,23 +13,23 @@ function NavBar() {
                         alt="Viettel logo"
                     />
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/products">Products</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/products">Products</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
                     </Nav>
-                    <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="primary">Search</Button>
-                    </Form>
+                    {/* <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="primary">Search</Button>
+                    </Form> */}
                 </Navbar.Collapse>
             </Navbar>              
         </div>
