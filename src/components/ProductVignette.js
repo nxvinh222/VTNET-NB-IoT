2d7cb328d26f6ea263f591137749ba358e37a5f6
unknown <nxvinh222@gmail.com>
2020-09-03 16:09:46 +0700

    Fix: design

--- src/components/ProductVignette.js
+++ src/components/ProductVignette.js
@@ -1,22 +1,32 @@
 import React from "react"
 import { Col } from "react-bootstrap"
+import { Link } from 'react-router-dom'
 import 'styles/ProductVignette.css'
 
 function ProductVignette(props) {
+
    return (
       <div className="vignette">
          <Col>
-            <div className="vignette-container"
-               style={{
-                  backgroundImage: "url(https://testwebsite.sodaq.com/wp-content/uploads/2016/12/3-scaled.jpg)"
-               }}>
-                  <div className="vignette-content">
-                     <h5>Lorem Ipsum</h5>
-                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget sollicitudin eros.
-                     </p>
-                  </div>
-            </div>  
+            <Link to={'products' + '/1'}>
+               <div className="vignette-container"
+                  onClick={"window.location.href = 'http://example.com';"}
+                  style={{
+                     backgroundImage: "url(https://testwebsite.sodaq.com/wp-content/uploads/2016/12/3-scaled.jpg)"
+                  }}
+                  
+               >
+                     <div className="vignette-content">
+                        <h5>Lorem Ipsum</h5>
+                        <p>
+                           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget sollicitudin eros.
+                        </p>
+                        <div className="continue-button">
+                           <i className="fas fa-arrow-circle-right"></i>
+                        </div>
+                     </div>
+               </div>  
+            </Link>
          </Col>
       </div>
       
