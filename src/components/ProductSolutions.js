973f9835ec75025bcf78a43640b4626e1356d034
unknown <nxvinh222@gmail.com>
2020-09-03 17:25:32 +0700

    Add: SolutionCard

--- src/components/ProductSolutions.js
+++ src/components/ProductSolutions.js
@@ -1,15 +1,38 @@
 import React, { useEffect, useState } from "react"
-import { Container, Row } from "react-bootstrap"
+import { Container, Row, Col } from "react-bootstrap"
+import 'styles/SolutionCard.css'
+
 
 import ProductVignette from 'components/ProductVignette'
 
 function ProductVignettes() {
 
+   const data = useState([])
+   const display = useState(0)
 
    return (
-      <div className="product-vignettes">
+      <div className="product-content">
          <Container>
-
+               <div className="solution-cards">
+                  <Row md={3}>
+                     <Col>
+                        <div className="solution-card-cnt">
+                           test
+                        </div>
+                     </Col>
+                     <Col>
+                        <div className="solution-card-cnt">
+                           test
+                        </div>
+                     </Col>
+                     <Col>
+                        <div className="solution-card-cnt">
+                           test
+                        </div>
+                     </Col>
+                     
+                  </Row>
+               </div>
                <Row xs={1} sm={1} md={1} lg={3}>
                   <ProductVignette />
                   <ProductVignette />
