5f18ab823af14cdf029b4294882c4b1e26e78376
unknown <nxvinh222@gmail.com>
2020-09-10 10:13:59 +0700

    Add: LoginPopup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,14 @@
-import React from 'react'
-import { Navbar, Nav, NavDropdown, Button } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Navbar, Nav, NavDropdown, Button, Modal } from 'react-bootstrap'
+import LoginPopup from 'components/LoginPopup'
 
 function NavBar() {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return(
         <div className="navbar-top">
             <Navbar bg="light" expand="sm">
@@ -27,10 +34,45 @@ function NavBar() {
                         </NavDropdown>
                     </Nav>
                     <div className="login-button md-text-right">
-                        <Button href="" variant="info" data-toggle="modal" data-target="#signupPage">
-                            Login<i class="fas fa-user-plus ml-3"></i>
+                        <Button variant="info"  onClick={handleShow}>
+                            Login<i className="fas fa-user-plus ml-3"></i>
                         </Button>
                     </div>
+                    <LoginPopup show={show} onHide={handleClose} />
+                    {/* <Modal show={show} onHide={handleClose}>
+                        <div class="modal-dialog">
+                            <div class="modal-content"> 
+                                <div class="modal-header text-center">
+                                <h3 class="modal-title w-100 dark-grey-text font-weight-bold">Sign In</h3>
+                                <button type="button" class="close" data-dismiss="modal" aria-lable="close">&times;</button>
+                                </div>
+
+                                <div class="modal-body mx-4">
+                                <div class="md-form">
+                                    <input type="email" class="form-control validate" />
+                                    <label data-error="wrong" data-success="right">Your Email</label>
+                                </div>
+
+                                <div class="md-form">
+                                    <input type="password" class="form-control validate" />
+                                    <label data-error="wrong" data-success="right">Your Password</label>
+                                    <p class="font-small blue-text d-flex justify-content-end">Forgot<a href="#" class="blue-text ml-1">Password?</a></p>
+                                </div>
+
+                                <div class="text-center mb-3">
+                                    <button type="button" class="btn btn-primary btn-block z-depth-1a">Sign in</button>
+                                </div>
+                                <p class="font-small dark-grey-text d-flex justify-content-center">or sign in with:</p>
+
+                                <div class="row my-3 justify-content-center">
+                                    <button type="button" class="btn btn-primary z-depth-1a"><i class="fab fa-facebook-f text-center"></i></button>
+                                    <button type="button" class="btn btn-purple z-depth-1a"><i class="fab fa-twitter text-center"></i></button>
+                                    <button type="button" class="btn btn-red z-depth-1a"><i class="fab fa-google-plus-g text-center"></i></button>
+                                </div>
+                                </div>
+                            </div>
+                        </div> 
+                    </Modal> */}
                 </Navbar.Collapse>
 
             </Navbar>              
