f53acc50d7584003a47aa7ac0ec448078c2e16d6
unknown <nxvinh222@gmail.com>
2020-09-10 08:47:20 +0700

    Fix: pseudodata

--- src/PseudoData/ProductVignette.js
+++ src/PseudoData/ProductVignette.js
@@ -25,19 +25,19 @@ const data = {
       [
          {
             title: "Lorem Ipsum",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget sollicitudin eros.",
+            content: "Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum",
             picture: "https://static.bhphotovideo.com/explora/sites/default/files/styles/top_shot/public/ts-space-sun-and-solar-viewing-facts-versus-fiction.jpg?itok=gaBs6QMS",
             productId: "00"
          },
          {
             title: "Lorem Ipsum Blue",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget sollicitudin eros.",
-            picture: "https://s7d2.scene7.com/is/image/TWCNews/lightning_png",
+            content: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+            picture: "https://i2.wp.com/www.omanobserver.om/wp-content/uploads/2018/11/Sea.jpg?fit=800%2C550&ssl=1",
             productId: "99"
          },
          {
             title: "Lorem Ipsum",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget sollicitudin eros.",
+            content: "Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, and going through the cites of the word in classical literature, discovered the undoubtable source.",
             picture: "https://images.pexels.com/photos/1118869/pexels-photo-1118869.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
             productId: "00"
          }
@@ -67,19 +67,19 @@ const data = {
       [
          {
             title: "Lorem Ipsum",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget sollicitudin eros.",
-            picture: "https://testwebsite.sodaq.com/wp-content/uploads/2016/12/3-scaled.jpg",
+            content: "Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum",
+            picture: "https://www.treeoftheyear.org/getmedia/431957b9-9809-4948-9f28-666b3a1a78b1/129633a;.aspx?width=1600",
             productId: "00"
          },
          {
             title: "Lorem Ipsum Blue",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget sollicitudin eros.",
+            content: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
             picture: "https://inside-our-products.loreal.com/sites/default/files/styles/banner/public/2020-05/Header-sun.jpg?itok=a9w2zzpA",
             productId: "99"
          },
          {
             title: "Lorem Ipsum",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget sollicitudin eros.",
+            content: "Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, and going through the cites of the word in classical literature, discovered the undoubtable source.",
             picture: "https://i.cbc.ca/1.5387796.1575670219!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/white-dwarf-and-exoplanet.jpg",
             productId: "00"
          }
